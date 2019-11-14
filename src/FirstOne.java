c18adbafbe6efa4f94230a1a748d3d4f5cca678e
ruslan <Lepak1987@gmail.com>
2019-11-13 21:51:15 -0600

    new message

--- src/FirstOne.java
+++ src/FirstOne.java
@@ -1,6 +1,6 @@
 public class FirstOne {
     public static void main(String[] args) {
         System.out.println("Hello");
-        System.out.println("Hi from Venera");
+        System.out.println("Hi from Ruslan");
     }
 }
